6575b805aad606d62b52bdd645a9ab5c15376f34
Luc Morey <lucmorey@gmail.com>
2018-02-22 17:59:28 -0800

    mongoDB working

--- server.js
+++ server.js
@@ -3,28 +3,45 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-    {name:'Tim', message: 'hi'},
-    {name: 'Jane', message: 'hello'}
-]
+var dbUrl = 'mongodb://user:user@ds245518.mlab.com:45518/node-chat'
+
+var Message = mongoose.model('message', {
+    name: String,
+    message: String
+})
+
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) =>{
+        res.send(messages)
+
+    })
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if (err) 
+                sendStatus(500)
+
+            io.emit('message', req.body)
+            res.sendStatus(200)     
+    })
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('MongoDB connection', err)
 })
 
 var server = http.listen(3000, () => {
